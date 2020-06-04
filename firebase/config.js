ea812d0ab35902bd8fbab0aa347fc90b61131c2e
Evan LeNoir <EvanDLeNoir@gmail.com>
2020-06-03 21:25:19 -0500

    Firebase added and updated

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyAIU26Kkms-fYkK6pxpCUYMiu8nQ-DgiMI",
+  authDomain: "apr2020-practice-spa.firebaseapp.com",
+  databaseURL: "https://apr2020-practice-spa.firebaseio.com",
+  projectId: "apr2020-practice-spa",
+  storageBucket: "apr2020-practice-spa.appspot.com",
+  messagingSenderId: "333949426243",
+  appId: "1:333949426243:web:74b90793255bdaa5e512f6",
+  measurementId: "G-138X6MB16L"
+};
